e37c7462d186973942ec5017271f5baf9c1c5c6a
Lukas Heppe <lukas.heppe@tu-dortmund.de>
2020-05-19 12:05:09 +0200

    Revert

--- DLplatform/communicating/rabbitMQComm.py
+++ DLplatform/communicating/rabbitMQComm.py
@@ -1,6 +1,7 @@
 import pickle
 import sys
 from collections import deque
+from math import ceil
 from typing import List
 
 import pika
@@ -350,44 +351,44 @@ class RabbitMQComm(Communicator):
         _base_topic = 'newModel.'
         _short_string_byte_limit = 255
 
-        topics = greedy(identifiers)
+        # topics = greedy(identifiers)
 
         # Compute available bytes for identifier
-        # _avail_bytes = _short_string_byte_limit - utf8len(_base_topic)
-        # _id_string = '.'.join(identifiers)
-        # _id_string_size = utf8len(_id_string)
-        #
-        # n_messages = ceil(_id_string_size / _avail_bytes)
-        #
-        # _topics = []
-        #
-        # for _m_idx in range(n_messages):
-        #     _id_string_size = utf8len(_id_string)
-        #
-        #     # Last message, we just pick the rest and continue
-        #     if _m_idx == n_messages - 1:
-        #         _topics.append(_id_string)
-        #         break
-        #
-        #     _split_idx_guess = min(_avail_bytes, _id_string_size)
-        #
-        #     # Check if _split_idx_guess is a valid split point
-        #     while _id_string[_split_idx_guess] != '.':
-        #         # Otherwise fix it by going back
-        #         _split_idx_guess = _split_idx_guess - 1
-        #
-        #     _topics.append(_id_string[:_split_idx_guess])
-        #
-        #     _id_string = _id_string[_split_idx_guess:]
-        #     # Cut off '.' since its already included in base topic
-        #     if _id_string[0] == '.':
-        #         _id_string = _id_string[1:]
+        _avail_bytes = _short_string_byte_limit - utf8len(_base_topic)
+        _id_string = '.'.join(identifiers)
+        _id_string_size = utf8len(_id_string)
+
+        n_messages = ceil(_id_string_size / _avail_bytes)
+
+        _topics = []
+
+        for _m_idx in range(n_messages):
+            _id_string_size = utf8len(_id_string)
+
+            # Last message, we just pick the rest and continue
+            if _m_idx == n_messages - 1:
+                _topics.append(_id_string)
+                break
+
+            _split_idx_guess = min(_avail_bytes, _id_string_size)
+
+            # Check if _split_idx_guess is a valid split point
+            while _id_string[_split_idx_guess] != '.':
+                # Otherwise fix it by going back
+                _split_idx_guess = _split_idx_guess - 1
+
+            _topics.append(_id_string[:_split_idx_guess])
+
+            _id_string = _id_string[_split_idx_guess:]
+            # Cut off '.' since its already included in base topic
+            if _id_string[0] == '.':
+                _id_string = _id_string[1:]
 
         message = pickle.dumps({'param': param,
                                 'flags': flags})
         message_size = sys.getsizeof(message)
 
-        for _topic in topics:
+        for _topic in _topics:
             topic = 'newModel.' + _topic
             self._publish(self._exchangeNodes, topic, message)
             self.learningLogger.logSendModelMessage(self._exchangeNodes, topic, message_size, 'send')
